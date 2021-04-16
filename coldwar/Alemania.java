1a09f380e1edd94b3222d5ff2e030323c26de1cf
klaudiusmiskinis <73123094+klaudiusmiskinis@users.noreply.github.com>
2021-04-16 16:12:05 +0200

    3 nuevas clases ESPANYA, ALEMANIA y VIETNAM

---
+++ coldwar/Alemania.java
@@ -0,0 +1,21 @@
+
+public class Alemania extends Paises{
+	//ATRIBUTOS
+	
+	//CONSTRUCTOR VACÍO
+	public Alemania() {
+		super();
+	}
+	
+	//CONSTRUCTOR CON PARAMETROS
+	public Alemania(String nombre, int vidas, int misiles, String tipo) {
+		super(nombre, vidas, misiles, tipo);
+	}
+	
+	//MÉTODOS
+	
+	//GETTERs
+	
+	//SETTERs
+
+}
---
+++ coldwar/Vietnam.java
@@ -0,0 +1,20 @@
+
+public class Vietnam extends Paises{
+	//ATRIBUTOS
+	
+	//CONSTRUCTOR VACIÓ
+	public Vietnam() {
+		super();
+	}
+	
+	//CONSTRUCTOR CON PARAMETROS
+	public Vietnam(String nombre, int vidas, int misiles, String tipo) {
+		super(nombre, vidas, misiles, tipo);
+	}
+	
+	//MÉTODOS
+	
+	//GETTERs
+	
+	//SETTERs
+}
